multi-os-engine/libcore

Move GC daemon locking logic into heap
Fixes deadlock caused by acquirng the mutator lock while
synchronizing on the daemon thread.

Bug: 18739541
Change-Id: Ib3ac3788081d3d471195a6e3a8ed163237616a4f

libart/src/main/java/dalvik/system/VMRuntime.java
@@ -297,6 +297,8 @@ public final class VMRuntime {
 
     public native void trimHeap();
     public native void concurrentGC();
+    public native void requestGC();
+    public native void waitForConcurrentGCRequest();
 
     /**
      * Let the heap know of the new process state. This can change allocation and garbage collection

SOKP/platform_libcore

libart/src/main/java/dalvik/system/VMRuntime.java
@@ -293,6 +293,8 @@ public final class VMRuntime {
 
     public native void trimHeap();
     public native void concurrentGC();
+    public native void requestConcurrentGC();
+    public native void waitForConcurrentGCRequest();
 
     /**
      * Let the heap know of the new process state. This can change allocation and garbage collection